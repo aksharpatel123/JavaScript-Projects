485a830ee0b3923f9bf3ede541e3aed42a79eb3e
Akshar Patel <49744200+aksharpatel123@users.noreply.github.com>
2020-11-09 10:07:20 -0600

    Update app.js

--- BudgetPlanner/app.js
+++ BudgetPlanner/app.js
@@ -110,7 +110,11 @@ var UIController = (function () {
         inputValue: '.add__value',
         inputBtn: '.add__btn',
         incomeContainer: '.income__list',
-        expenseContainer: '.expenses__list'
+        expenseContainer: '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expensesLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage'
 
     }
 
@@ -168,6 +172,23 @@ var UIController = (function () {
             fieldsArr[0].focus();
         },
 
+        // Update Budgeet UI
+        displayBudget: function(obj) {
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+
+            // Update the percentage only if its more than 0, if 0 or neg display '---' 
+            // This is the case when first value entered is exp (-)
+            if (obj.percentage > 0) {
+                document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+            }
+            else {
+                document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+            }
+            
+        },
+
         // returns the DOMstrings object for access to other components
         getDOMstrings: function() {
             return DOMstrings;
@@ -202,7 +223,7 @@ var controller = (function (budgetCtrl, UICtrl) {
         var budget = budgetCtrl.getBudget();
 
         // 3. Display the budget on the UI
-        console.log(budget);
+        UICtrl.displayBudget(budget);
     };
 
     // This is called everytime when a user enters new item.
@@ -233,9 +254,16 @@ var controller = (function (budgetCtrl, UICtrl) {
 
     // setupEventListeners will not get executed, needs to be called.
     // Init to call that function.
+    // Init runs when app is started
     return {
         init: function() {
-            console.log('App has started.');
+            console.log('Application has started.');
+            UICtrl.displayBudget({
+                budget: 0,
+                totalInc: 0,
+                totalExp: 0,
+                percentage: -1
+            }); // pass an object with all 0 values
             setupEventListeners();
         }
     };
